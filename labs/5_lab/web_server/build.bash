b6b308819a9df39a5fcf04e52a21bc3df26a032e
VeraKasianenko <verakasianenko02@gmail.com>
2023-07-19 19:53:05 +0300

    comments 5lab

--- labs/5_lab/web_server/build.bash
+++ labs/5_lab/web_server/build.bash
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 sudo docker build -t server-image .
+
+# выполняет сборку Docker-образа с помощью Dockerfile, образ будет назван "server-image"
--- labs/5_lab/web_server/exec.bash
+++ labs/5_lab/web_server/exec.bash
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Получаем ID контейнера с именем "webserver"
 id=$(sudo docker container ls --all --quiet --filter "name=webserver")
+
+# Запускаем интерактивную оболочку bash внутри контейнера с указанным ID
 docker exec -it $id bash
